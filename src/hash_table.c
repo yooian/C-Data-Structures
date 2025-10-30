ae5ee518c9ebe02652938b556f8bb7bacbaf6c53
Ian Yoo <isyoo@ucdavis.edu>
2025-10-29 20:46:58 -0700

    Add find slot logic

--- src/hash_table.c
+++ src/hash_table.c
@@ -37,10 +37,34 @@ struct HashTable
 // Idea: just a workhorse func that searches for key in entry list. Returns pointer to a slot
 static HashTableEntry *ht_find_slot(HashTable *table, const void *key, HashTableEntry **p_first_tombstone)
 {
+    size_t hash = table->hash_fn(key);
     // go through entries
-    // if we're searching, we just look for matching key and return either slot (if found) or NULL/empty
-    // if we're deleting, same thing. In the delete func, we set the status to deleted
-    // if inserting, same thing but we store any passing tombstones. In insert func, we insert into either first tombstone or empty
+    for (size_t i = 0; i < table->capacity; ++i)
+    {
+        // i*i is simplest quadratic probe
+        size_t index = (hash + i * i) % table->capacity;
+        HashTableEntry *entry = &table->entries[index];
+
+        if (entry->state == SLOT_OCCUPIED)
+        {
+            // if we're searching, we just look for matching key and return either slot (if found) or NULL/empty
+            // if we're deleting, same thing. In the delete func, we set the status to deleted
+            if (table->comp_fn(entry->key, key) == 0)
+            {
+                return entry;
+            }
+        }
+        else if (entry->state == SLOT_DELETED)
+        {
+            // if inserting, same thing but we store any passing tombstones. In insert func, we insert into either first tombstone or empty
+            if (p_first_tombstone != NULL && *p_first_tombstone == NULL)
+                *p_first_tombstone = entry;
+        }
+        else if (entry->state == SLOT_EMPTY)
+        {
+            return entry;
+        }
+    }
 }
 
 typedef size_t (*HashFunction)(const void *key);
